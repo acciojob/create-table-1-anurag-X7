ba25d34f072ef38f73e24a5e0f6a4c3981a90a9f
anurag-X7 <168930931+anurag-X7@users.noreply.github.com>
2025-02-19 14:50:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 function insert_Row() {
-    //Write your code here
-  
+    var table = document.getElementById("sampleTable");
+    var newRow = table.insertRow(0); // Adds the row at the top of the table
+    
+    // Insert new cells into the row
+    var cell1 = newRow.insertCell(0);
+    var cell2 = newRow.insertCell(1);
+
+    // Fill the new cells with content
+    cell1.innerHTML = "New Cell1";
+    cell2.innerHTML = "New Cell2";
+}
+	
   
 }
